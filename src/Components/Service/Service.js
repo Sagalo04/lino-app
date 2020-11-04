93644552b85fbbb728cca329e950309c8072a7e4
Santiago Garcna <santiago.garcna@uao.edu.co>
2020-11-03 19:43:45 -0500

    Component Datepicker

--- src/Components/Service/Service.js
+++ src/Components/Service/Service.js
@@ -1,28 +1,15 @@
 import React from 'react';
-import DateFnsUtils from '@date-io/date-fns';
-import { makeStyles } from '@material-ui/core/styles';
-import { FormControl, InputLabel, MenuItem, Select } from '@material-ui/core';
 import './Service.css'
-import {
-    KeyboardDatePicker, MuiPickersUtilsProvider,
-} from '@material-ui/pickers';
 import ServiceHeader from './ServiceHeader/ServiceHeader';
 import OButton from '../OButton/OButton'
 import { Link } from 'react-router-dom';
 //service Select
 import ServiceSelect from './ServiceSelect/ServiceSelect';
+import ServiceDatePicker from './ServiceDatePicker/ServiceDatePicker';
 
 
 function Service(props) {
-
-    const [selectedDate, setSelectedDate] = React.useState(new Date());
-
-    const handleDateChange = (date) => {
-        setSelectedDate(date);
-    };
-
-    
-
+   
     return (
         <div className="o-service">
             <ServiceHeader />
@@ -32,20 +19,7 @@ function Service(props) {
             {/*Seleccionar especialidad*/}
             <ServiceSelect label={"Especialidad:"} initialValue={"General"} options={["General","Cardiólogo"]}/>
             {/*Seleccionar fecha*/}
-            <div className="o-select">
-                <p className='o-label'>Para el dia: </p>
-                <MuiPickersUtilsProvider className="o-mui" utils={DateFnsUtils} >
-                    <KeyboardDatePicker
-                        className="mydatepicker"
-                        clearable
-                        format="dd/MM/yyyy"
-                        width='20'
-                        label="Día de la cita"
-                        value={selectedDate}
-                        onChange={handleDateChange}
-                    />
-                </MuiPickersUtilsProvider>
-            </div>
+            <ServiceDatePicker />
             <hr />
             {/*Boton para confimar servicio*/}
             <Link to="/Servicio">
