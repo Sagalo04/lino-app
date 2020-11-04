9a1febec23a135d165bb461cc8f4ced948732352
Gabriel Andrés Mercado Rivera <gabriel.mercado@uao.edu.co>
2020-11-04 14:39:04 -0500

    se arregla el mapa del fondo

--- src/Components/Service/Service.js
+++ src/Components/Service/Service.js
@@ -9,22 +9,24 @@ import ServiceDatePicker from './ServiceDatePicker/ServiceDatePicker';
 
 
 function Service(props) {
-   
+
     return (
-        <div className="o-service">
-            <ServiceHeader />
-            {/*Seleccionar Medico/Psicologo*/}
-            <ServiceSelect label={"Deseo un:"} initialValue={"Médico/Psicólogo"} options={["Médico","Psicólogo"]}/>
-            
-            {/*Seleccionar especialidad*/}
-            <ServiceSelect label={"Especialidad:"} initialValue={"General"} options={["General","Cardiólogo"]}/>
-            {/*Seleccionar fecha*/}
-            <ServiceDatePicker />
-            <hr />
-            {/*Boton para confimar servicio*/}
-            <Link to="/Servicio">
-                <OButton label={"Aceptar"}></OButton>
-            </Link>
+        <div className="o-body">
+            <div className="o-service">
+                <ServiceHeader />
+                {/*Seleccionar Medico/Psicologo*/}
+                <ServiceSelect label={"Deseo un:"} initialValue={"Médico/Psicólogo"} options={["Médico", "Psicólogo"]} />
+
+                {/*Seleccionar especialidad*/}
+                <ServiceSelect label={"Especialidad:"} initialValue={"General"} options={["General", "Cardiólogo"]} />
+                {/*Seleccionar fecha*/}
+                <ServiceDatePicker />
+                <hr />
+                {/*Boton para confimar servicio*/}
+                <Link to="/Servicio">
+                    <OButton label={"Aceptar"}></OButton>
+                </Link>
+            </div>
         </div>
     );
 }
--- src/Components/Service/ServiceConfirm.js
+++ src/Components/Service/ServiceConfirm.js
@@ -8,31 +8,33 @@ import DocCard from './DocCard/DocCard';
 
 function ServiceConfirm(props) {
     return (
-        <div className="o-service">
-            <ServiceConfirmHeader title="¡Hemos encontrado tu servicio!"/>
-            <div className="o-service-contain">
-                <DocCard 
-                    name="Javier Antonio Vazques"
-                    info="Médico Cardiólogo"
-                    sourceImg="https://material-ui.com/static/images/avatar/1.jpg"
-                />
-                <div className="o-service-info">
-                    <p>Fecha:</p>
-                    <p>29/09/2020</p>
-                </div>
-                <div className="o-service-info">
-                    <p>Hora estimada de llegada:</p>
-                    <p>9:00 AM</p>
-                </div>
-                <div className="o-service-info">
-                    <p>Precio de consulta:</p>
-                    <p>$35.000</p>
+        <div className="o-body">
+            <div className="o-service">
+                <ServiceConfirmHeader title="¡Hemos encontrado tu servicio!" />
+                <div className="o-service-contain">
+                    <DocCard
+                        name="Javier Antonio Vazques"
+                        info="Médico Cardiólogo"
+                        sourceImg="https://material-ui.com/static/images/avatar/1.jpg"
+                    />
+                    <div className="o-service-info">
+                        <p>Fecha:</p>
+                        <p>29/09/2020</p>
+                    </div>
+                    <div className="o-service-info">
+                        <p>Hora estimada de llegada:</p>
+                        <p>9:00 AM</p>
+                    </div>
+                    <div className="o-service-info">
+                        <p>Precio de consulta:</p>
+                        <p>$35.000</p>
+
+                    </div>
+                    <hr />
+
+                    <Link><OButton label={"Confirmar"}></OButton></Link>
 
                 </div>
-                <hr />
-
-                <Link><OButton label={"Confirmar"}></OButton></Link>
-
             </div>
         </div>
     );
