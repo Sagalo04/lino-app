9f3f3ce6d558eadfb25ff0435e9fac64911c7bde
Santiago Garcna <santiago.garcna@uao.edu.co>
2021-08-10 11:56:11 -0500

    Tiomer update

--- src/Components/Service/ServiceStarted/Timer/Timer.js
+++ src/Components/Service/ServiceStarted/Timer/Timer.js
@@ -1,38 +1,49 @@
-import React, {useState, useEffect} from 'react';
-import './Timer.css'
+import React, { useState, useEffect } from "react";
+import "./Timer.css";
 
-const Timer = ()=>{
-    const [time, setTime] = useState({s:0, m:0, h:0});
+const Timer = () => {
+  const [time, setTime] = useState({
+    s: 0,
+    m: 0,
+    h: 0,
+  });
 
-    //on mount
-    useEffect(_=>{
-        run();
-        setInterval(run, 1000);
-    }, [])
-    
-    var updatedM = time.m, updatedS = time.s, updatedH = time.h
+  //on mount
+  useEffect((_) => {
+    run();
+    setInterval(run, 1000);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
 
-    const run = ()=>{
-        if(updatedM === 60){
-            updatedH ++;
-            updatedM = 0;
-        }
-        if(updatedS === 60){
-            updatedM ++;
-            updatedS = 0;
-        }
-        updatedS++;
-        setTime({s: updatedS, m: updatedM, h:updatedH});
+  var updatedM = time.m,
+    updatedS = time.s,
+    updatedH = time.h;
+
+  const run = () => {
+    if (updatedM === 60) {
+      updatedH++;
+      updatedM = 0;
     }
+    if (updatedS === 60) {
+      updatedM++;
+      updatedS = 0;
+    }
+    updatedS++;
+    setTime({
+      s: updatedS,
+      m: updatedM,
+      h: updatedH,
+    });
+  };
 
-    return(
-        <div className="o-time-counter">
-            {`${time.h < 10? `0${time.h}`: time.h}
-            :${time.m < 10? `0${time.m}`: time.m}
-            :${time.s < 10? `0${time.s}`: time.s}`}
-        </div>
-    )
-}
-
+  return (
+    <div className="o-time-counter">
+      {" "}
+      {`${time.h < 10 ? `0${time.h}` : time.h}
+            :${time.m < 10 ? `0${time.m}` : time.m}
+            :${time.s < 10 ? `0${time.s}` : time.s}`}{" "}
+    </div>
+  );
+};
 
 export default Timer;
